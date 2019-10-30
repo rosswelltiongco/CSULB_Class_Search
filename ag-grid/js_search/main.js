7ffb4d4d34d5076cfb1c919ba77e1474c1c0960b
Rosswell Tiongco <rosswelltiongco@gmail.com>
2019-10-30 00:23:58 -0700

    Chagned variable names for second grid

--- ag-grid/js_search/main.js
+++ ag-grid/js_search/main.js
@@ -1,4 +1,4 @@
-var columnDefs = [
+var columnDefs1 = [
     {headerName: "#", field: "CLASS_NUMBER", width: 90, filter: 'agTextColumnFilter', filterParams:{
         filterOptions:['contains'],
         debounceMs:0,
@@ -24,30 +24,30 @@ var columnDefs = [
     }}
 ];
 
-var gridOptions = {
+var gridOptions1 = {
     defaultColDef: {
         sortable: true,
         filter: true
     },
-    columnDefs: columnDefs,
+    columnDefs: columnDefs1,
     rowData: null,
     floatingFilter:true
 };
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
-    var gridDiv = document.querySelector('#myGrid');
-    new agGrid.Grid(gridDiv, gridOptions);
+    var gridDiv1 = document.querySelector('#myGrid1');
+    new agGrid.Grid(gridDiv1, gridOptions1);
 
     // do http request to get our sample data - not using any framework to keep the example self contained.
     // you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.open('GET', 'https://raw.githubusercontent.com/rosswelltiongco/CSULB_Class_Search/master/data.json');
-    httpRequest.send();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
-            var httpResult = JSON.parse(httpRequest.responseText);
-            gridOptions.api.setRowData(httpResult);
+    var httpRequest1 = new XMLHttpRequest();
+    httpRequest1.open('GET', 'https://raw.githubusercontent.com/rosswelltiongco/CSULB_Class_Search/master/data.json');
+    httpRequest1.send();
+    httpRequest1.onreadystatechange = function() {
+        if (httpRequest1.readyState === 4 && httpRequest1.status === 200) {
+            var httpResult1 = JSON.parse(httpRequest1.responseText);
+            gridOptions1.api.setRowData(httpResult1);
         }
     };
 });
