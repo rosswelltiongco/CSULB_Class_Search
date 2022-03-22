7d2f864b83a4acbb496c22d82fbed211956b5213
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-22 17:15:24 +0800

    Delete ClassSearch.py

--- ClassSearch.py
+++
@@ -1,8 +0,0 @@
-from lib.Database import *
-from lib.Time import *
-
-class ClassSearch:
-    def __init__(self):
-        self.database = Database()
-        self.time = Time()
-        #print("IT IS CURRENTLY: {}\nDAY: {}".format(self.time.get_time(), self.time.get_day()))
